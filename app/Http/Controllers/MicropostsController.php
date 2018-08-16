945cd10d9338e2142260e137e3d14db3106711cd
inaronete <be.here.now1217@gmail.com>
2018-08-16 11:16:15 +0000

    post

---
+++ app/Http/Controllers/MicropostsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MicropostsController extends Controller
+{
+    public function index()
+    {
+        $data = [];
+        if (\Auth::check()) {
+            $user = \Auth::user();
+            $microposts = $user->maicroposts()->orderBy('created_at', 'desc')->paginate(10);
+            
+            $data = [
+                'user' => $user,
+                'microposts' => $microposts,
+                ];
+                $data += $this->counts($user);
+                return view('users.show',$data);
+        }else {
+            return view('welcome');
+        
+        }
+    }
+}
