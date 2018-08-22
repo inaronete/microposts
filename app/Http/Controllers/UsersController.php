c9fa5c2011a3b30906fe4f494b8364373b61810d
inaronete <be.here.now1217@gmail.com>
2018-08-22 04:20:10 +0000

    follow, unfollow

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,8 @@ namespace   App\Http\Controllers;
 
 use App\User;
 
+use App\Micropost;
+
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
@@ -29,5 +31,35 @@ class UsersController extends Controller
         $data += $this->counts($user);
         
         return view('users.show',$data);
+    }
+    
+    public function followings($id)
+    {
+        $user = User::find($id);
+        $followings = $user->followings()->paginate(10);
+        
+        $data = [
+            'user' => $user,
+            'users' => $followings,
+        ];
+        
+        $data += $this->counts($user);
+        
+        return view('users.followings', $data);
+    }
+    
+    public function followers($id)
+    {
+        $user = User::find($id);
+        $followers = $user->followers()->paginate(10);
+        
+        $data = [
+            'user' => $user,
+            'users' => $followers,
+        ];
+        
+        $data += $this->counts($user);
+        
+        return view('users,followers', $data);
     }
 }
